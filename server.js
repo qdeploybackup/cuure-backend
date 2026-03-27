cff91010e411a63c4bd86f79a2bd8e65ce815eeb
benakackp <benakackp469@gmail.com>
2026-03-27 17:40:52 +0530

    connected with frontend

--- server.js
+++ server.js
@@ -11,7 +11,13 @@ const websiteRoute = require("./routes/website.route");
 const app = express();
 
 // app.use(cors({ origin: "http://localhost:3039", credentials: true }));
-app.use(cors({ origin: "http://localhost:5173","https:cuure.health", credentials: true}));
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://cuure.health"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
